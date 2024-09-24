90820676ec1e5f5c8a3750fdb1fb6f9ad2c13a0d
14kviruz <jzeballoslopes@gmail.com>
2024-09-24 12:36:47 -0400

    segundo commit

--- rutas/rutas.php
+++ rutas/rutas.php
@@ -1,17 +1,23 @@
 <?php
 //capturamos la ruta
-$array = explode("/",$_SERVER["REQUEST_URI"]);
+$array = explode("/", $_SERVER["REQUEST_URI"]);
 
-if(count(array_filter($array))==1){
-$json=array(
+/*================================
+cuando no se hace ninguna peticion a la API
+======================================= */
+if (count(array_filter($array)) == 1) {
     $json = array(
         "Detalle" => "Sin Solicitudes",
-    )
-)
-}else{
-    if(count(array_filter($array))==2){
-        $json=array(
-                "Detalle" => "Con Solicitudes",
-)}
+    );
+    echo json_encode($json, true);
+
+} else {
+    /*================================
+cuando si se hace peticion a la API
+======================================= */
+    if (count(array_filter($array)) == 2) {
+        $json = array(
+            "Detalle" => "Con Solicitudes",
+        );
+    }
 }
-?>
